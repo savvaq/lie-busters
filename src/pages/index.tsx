41f435804cdf74d22e244346869ed2c49fac61eb
Dima Savchenko <savvaq@gmail.com>
2023-05-04 13:06:35 +0200

    added buttons to the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -14,6 +13,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="game-wrapper">
+        <button className="button" id="create-game-button">Create Game</button>
+        <button className="button" id="join-game-button">Join Game</button>
+      </div>
     </>
   )
 }
