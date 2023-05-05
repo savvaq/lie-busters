733bf09c88c6faabc1f5e70b695080ddfd21771f
Dima Savchenko <savvaq@gmail.com>
2023-05-05 12:32:18 +0200

    front pages styling changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Button from '../components/button/button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,10 +14,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="game-wrapper">
-        <button className="button" id="create-game-button">Create Game</button>
-        <button className="button" id="join-game-button">Join Game</button>
+      <div className={styles.gamewrapper}>
+        <Button text="Create Game" />
+        <Button text="Join Game" />
       </div>
     </>
+    // First button on click shows a modal to enter your name
+    // button to create a game
+    // on click it goes to another "Lobby" page
+
+    // Second button on click shows a modal to enter the code for the game
+    // two fields - one for name and one for code
+    // and submit button
   )
 }
