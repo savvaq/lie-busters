160279587dfa841cf5fada4c6b3b5f6bcbda26a5
Dima Savchenko <savvaq@gmail.com>
2023-05-07 20:27:29 +0200

    test of new branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Lying Game</title>
+        <title>Zalupa Konya</title>
         <meta name="description" content="Win by lying!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
