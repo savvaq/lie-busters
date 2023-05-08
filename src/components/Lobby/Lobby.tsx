6a2ceb0bfd5f7d8bc3b4be57b6ba8474c259d351
Dima Savchenko <savvaq@gmail.com>
2023-05-08 22:23:53 +0200

    Lobby styling changes

--- src/components/Lobby/Lobby.tsx
+++ src/components/Lobby/Lobby.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
 import { Player } from '@prisma/client';
 import { GameWithRelations } from '@/lib/types';
+import styles from './Lobby.module.css';
+import { sigmar } from '../../app/fonts';
+import Button from '../Button/Button';
 
 type LobbyProps = {
   game: GameWithRelations;
@@ -10,20 +13,22 @@ type LobbyProps = {
 
 const Lobby: FC<LobbyProps> = ({ game, isHost, startGame }) => {
   return (
-    <div>
-      <h1>Lobby</h1>
-      {game.players?.map((player: Player) => {
-        return <p key={player.id}>{player.name}</p>;
-      })}
-
+    <div className={styles['lobby-wrapper']}>
+      <h1 className={styles.title + " " +sigmar.className}>Lying Game</h1>
+      <h2 className={styles.description}>Waiting for host to start the game...</h2>
+      <h2 className={styles['players-header'] + " " +sigmar.className}>Players</h2>
+      <div className={styles['players-wrapper']}>
+        {game.players.map((player: Player) => (
+          <div key={player.id} className={styles['player-wrapper']}>
+            <div className={styles['player-icon']}></div>
+            <div className={styles['player-name-wrapper']}>
+              <p className={styles['player-name-text']}>{player.name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
       {isHost && (
-        <button
-          type="button"
-          onClick={startGame}
-          disabled={!game.players || game.players.length < 2}
-        >
-          Start game
-        </button>
+        <Button text="Start Game" onclick={startGame} disabled={!game.players || game.players.length < 2} />
       )}
     </div>
   );
