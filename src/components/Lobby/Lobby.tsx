5800bfc8e57d702ec8a6ac8663a07771a3bb79b3
Dima Savchenko <savvaq@gmail.com>
2023-05-09 14:37:58 +0200

    styling changes to the lobby page

--- src/components/Lobby/Lobby.tsx
+++ src/components/Lobby/Lobby.tsx
@@ -4,6 +4,7 @@ import { GameWithRelations } from '@/lib/types';
 import styles from './Lobby.module.css';
 import { sigmar } from '../../app/fonts';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 type LobbyProps = {
   game: GameWithRelations;
@@ -12,10 +13,26 @@ type LobbyProps = {
 };
 
 const Lobby: FC<LobbyProps> = ({ game, isHost, startGame }) => {
+
+  const [tooltip, setTooltip] = useState(false);
+
+  const showTooltip = () => {
+    setTooltip(true);
+    setTimeout(() => {
+      setTooltip(false);
+    }, 2000);
+  };
+
   return (
     <div className={styles['lobby-wrapper']}>
       <h1 className={styles.title + " " +sigmar.className}>Lying Game</h1>
-      <h2 className={styles.description}>Waiting for host to start the game...</h2>
+      <div className={styles['game-code-wrapper']}>
+        <p className={styles['game-code-text']}>Game Code</p>
+        <div onClick={() => {navigator.clipboard.writeText(game.code); showTooltip();}} className={styles['game-code-number']}>
+          {game.code}          
+          {tooltip && <div className={styles['tooltip']}>Copied!</div>}
+        </div>
+      </div>
       <h2 className={styles['players-header'] + " " +sigmar.className}>Players</h2>
       <div className={styles['players-wrapper']}>
         {game.players.map((player: Player) => (
@@ -27,9 +44,12 @@ const Lobby: FC<LobbyProps> = ({ game, isHost, startGame }) => {
           </div>
         ))}
       </div>
-      {isHost && (
-        <Button text="Start Game" onclick={startGame} disabled={!game.players || game.players.length < 2} />
-      )}
+      {
+      isHost ? 
+      <Button text="Start Game" onclick={startGame} disabled={!game.players || game.players.length < 2} />
+      :
+      <h2 className={styles.description}>Waiting for host to start the game...</h2>
+      }
     </div>
   );
 };
