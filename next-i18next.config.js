e6d368746e8b528cf6faf01d00f4414ca7e76928
Kovalchuk Andrii <andrii.kovalchuk@justeattakeaway.com>
2023-05-17 15:44:35 +0300

    Update i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+import path from 'node:path';
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
+    localePath: path.resolve('./public/locales'),
   },
-  react: { useSuspense: false },
 };
