d78c7ac642acf85a2b96771c2622d8c52b85efa1
Dima Savchenko <savvaq@gmail.com>
2023-05-20 18:21:28 +0200

    added maximum scale 1 so there's no zoom on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <Head>
         <title>Lying Game</title>
         <meta name="description" content="Win by lying!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
